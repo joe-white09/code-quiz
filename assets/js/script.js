8a578cfea66a52216df79511cef1053bbabd1766
joe-white09 <joe_white09@yahoo.com>
2022-05-14 12:49:57 -0500

    added points for correct, deduct time for incorrect

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,17 @@
-// pulling buttons to js
+// pulling elements to js
 var rules = document.querySelector("#rules-btn");
 var start = document.querySelector("#start-btn");
 var highScores = document.querySelector("#high-scores-btn");
 var timerEl = document.getElementById("countdown");
 var answers = document.querySelector("#answer-buttons");
-var question = document.querySelector("#question");
-
-var questionEl= document.createElement("span");
-var ansA = document.createElement("button");
-var ansB = document.createElement("button");
-var ansC = document.createElement("button");
-var ansD = document.createElement("button");
+var question = document.getElementById("question");
+var btnA = document.getElementById("btn-A");
+var btnB = document.getElementById("btn-B");
+var btnC = document.getElementById("btn-C");
+var btnD = document.getElementById("btn-D");
 
 
-questionEl.textContent = "";
-ansA.textContent = "";
-ansB.textContent = "";
-ansC.textContent = "";
-ansD.textContent = "";
+var timeLeft = 60;
 
 
 // functions of the buttons
@@ -26,19 +20,12 @@ var openRules = function(){
     document.getElementById("question-card").setAttribute("class", "hide")
 };
 
-var generateQuestions = function(){
-    question.appendChild(questionEl);
-    answers.appendChild(ansA);
-    answers.appendChild(ansB);
-    answers.appendChild(ansC);
-    answers.appendChild(ansD);
-};
-
 var startQuiz = function(){
     document.getElementById("rules-list").setAttribute("class", "hide");
     document.getElementById("question-card").removeAttribute("class", "hide");
-    countDown();
-    generateQuestions(quizQuestions);
+    for (var i = 0; i < quizQuestions.length; i++){
+      generateQuestion();
+        };   
 };
 
 var highScoresList = function(){
@@ -46,40 +33,127 @@ var highScoresList = function(){
     document.getElementById("question-card").setAttribute("class", "hide")
 };
 
-var countDown = function() {
-    var timeLeft = 60;
-    var timeInterval = setInterval(function () {
-      if (timeLeft > 1) {
-        timerEl.textContent = timeLeft + ' seconds remaining';
-        timeLeft--;
-      } 
-      else if (timeLeft === 1) {
-        timerEl.textContent = timeLeft + ' second remaining';
-        timeLeft--;
-      } 
-      else {
-        timerEl.textContent = '';
-        clearInterval(timeInterval);
-      }
-    }, 1000);
+let quizQuestionIndex =0;
+
+var generateQuestion = function() {
+  let q = quizQuestions[quizQuestionIndex];
+  question.innerHTML = "<h2>" + q.question + "<h2>";
+  btnA.innerHTML = q.btnA;
+  btnB.innerHTML = q.btnB;
+  btnC.innerHTML = q.btnC;
+  btnD.innerHTML = q.btnD;
 };
 
-var quizQuestions=[
-    question.textContent= "what does HTML stand for?",
-    ansA.textContent= "HyperText Markup Language",
-    ansB.textContent= "something else",
-    ansC.textContent= "another thing",
-    ansD.textContent= "thing",
+var checkAnswer = function(event){
 
+  if(event.target.getAttribute("id")===quizQuestions[quizQuestionIndex].correct){
+    playerInfo.score=playerInfo.score+5;
+    quizQuestionIndex++;
+    generateQuestion();
+    console.log(playerInfo.score);
+  }
+  else{
+    timeLeft=timeLeft-5;
+    quizQuestionIndex++;
+    generateQuestion();
+    console.log(playerInfo.score);
+  };
+};
+
+//Sets content for the question card
+var quizQuestions=[{
+  question: "what does HTML stand for?",
+  btnA:"HyperText Markup Language",
+  btnB:"HyperText Money Language",
+  btnC: "HotType Markup Language",
+  btnD:"HighType Markdown Language",
+  correct:"btn-A"
+}, 
+{
+  question:"What does CSS stand for?",
+  btnA:"Cascading Style Sheet",
+  btnB:"Creeping Style Sheet",
+  btnC:"Crawling Slang Style",
+  btnD:"Cascading Style Style",
+  correct: "btn-A"
+},
+{
+  question:"What is a Boolean Value",
+  btnA:"True",
+  btnB:"False",
+  btnC:"Correct",
+  btnD:"True or False",
+  correct:"btn-D"
+},
+{
+  question:"",
+  btnA:"",
+  btnB:"",
+  btnC:"",
+  btnD:"",
+  correct:""
+},
+{
+  question:"",
+  btnA:"",
+  btnB:"",
+  btnC:"",
+  btnD:"",
+  correct:""
+},
+{
+  question:"",
+  btnA:"",
+  btnB:"",
+  btnC:"",
+  btnD:"",
+  correct:""
+},
+{
+  question:"",
+  btnA:"",
+  btnB:"",
+  btnC:"",
+  btnD:"",
+  correct:""
+},
+{
+  question:"",
+  btnA:"",
+  btnB:"",
+  btnC:"",
+  btnD:"",
+  correct:""
+},
+{
+  question:"",
+  btnA:"",
+  btnB:"",
+  btnC:"",
+  btnD:"",
+  correct:""
+}
 ];
 
+//player info
+var playerInfo = {
+  name:"",
+  score:0
+};
 
 
 // event listeners
-rules.addEventListener('click', openRules);
-
+rules.addEventListener("click", openRules);
 start.addEventListener("click", startQuiz);
-
 highScores.addEventListener("click", highScoresList);
+btnA.addEventListener("click", checkAnswer);
+btnB.addEventListener("click", checkAnswer);
+btnC.addEventListener("click", checkAnswer);
+btnD.addEventListener("click", checkAnswer);
+
+
+
+
+
 
     
